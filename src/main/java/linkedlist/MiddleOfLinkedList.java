9747095fac1b5632dc8b4d5f94960021697e5b1f
akashdeep.dhanju <akashdeep.dhanju@payu.in>
2024-07-04 16:37:14 +0530

    linked list problems

--- src/main/java/linkedlist/MiddleOfLinkedList.java
+++ src/main/java/linkedlist/MiddleOfLinkedList.java
@@ -1,7 +1,13 @@
 package main.java.linkedlist;
 
+
 public class MiddleOfLinkedList {
   public static void main(String[] args) throws Exception {
 
   }
+
+  ListNode middleOfLinkedList(ListNode head) {
+    if (h)
+  }
+
 }
